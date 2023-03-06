9a16db366cb6f732b10ea2ab800a3ffb3799ad7e
TriremeSolutions <themellowread@gmail.com>
2023-03-06 02:16:23 -0800

    bettychecked all tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -15,19 +15,19 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *a, *b;
-	if (list == NULL || list -> next == NULL)
+listint_t *a, *b;
+	if (list == NULL || list->next == NULL)
 		return (0);
 
-	a = list -> next;
-	b = list -> next -> next;
+	a = list->next;
+	b = list->next->next;
 
-	while(a && b && a -> next)
+	while (a && b && a->next)
 	{
 		if (a == b)
 			return (1);
-		a = a -> next;
-		b = b -> next -> next;
+		a = a->next;
+		b = b->next->next;
 	}
 	return (0);
 }
