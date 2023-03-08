0d8aff17698dd4e47bf61978853fcdf3786730cf
TriremeSolutions <themellowread@gmail.com>
2023-03-07 16:09:22 -0800

    101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    cpy = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            cpy += str[i]
+    return cpy
