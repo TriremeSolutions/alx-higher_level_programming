99893376647d39feba3fbc7e1cfa3582fb7bfaab
TriremeSolutions <themellowread@gmail.com>
2023-03-12 12:50:39 -0700

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    chopped = [n for n in my_string if not (n != 'C' or n != 'c')]
+    chopped = [n for n in my_string if n != 'C' and n != 'c']
     return ("".join(chopped))
